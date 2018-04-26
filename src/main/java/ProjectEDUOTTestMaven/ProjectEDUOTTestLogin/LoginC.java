5048572e0a84b1cf5550f78bdc43abd2c587f687
jata2011 <jata2011@gmail.com>
2018-04-26 21:05:29 +1000

    Initial Commit 26th April

---
+++ src/main/java/ProjectEDUOTTestMaven/ProjectEDUOTTestLogin/LoginC.java
@@ -0,0 +1,17 @@
+package ProjectEDUOTTestMaven.ProjectEDUOTTestLogin;
+
+import org.testng.annotations.Test;
+
+public class LoginC {
+	@Test
+	public void firstTestCase()
+	{
+		System.out.println("im in first test case from LoginC Class");
+	}
+	
+	@Test
+	public void secondTestCase()
+	{
+		System.out.println("im in second test case from LoginC Class");
+	}
+}
---
+++ src/main/java/ProjectEDUOTTestMaven/ProjectEDUOTTestMaven/App.java
@@ -0,0 +1,13 @@
+package ProjectEDUOTTestMaven.ProjectEDUOTTestMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
